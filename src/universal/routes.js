9f52555e2d3d972e47a779188b8fdf7c52544ee0
Viral Ganatra <viral@viralganatra.com>
2018-05-11 17:31:26 +0100

    Remove routes folder to remove unnecessary nesting

--- src/universal/routes.js
+++ src/universal/routes.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import IndexPage from 'universal/pages/index-page';
 import NotFoundPage from 'universal/pages/not-found-page';
 
-export const Routes = () => (
+const Routes = () => (
   <Switch>
     <Route exact path="/" component={IndexPage} />
     <Route component={NotFoundPage} />
